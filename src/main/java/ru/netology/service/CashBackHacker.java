26c087d3d2a2fbc58e986230287678f8d238aada
Sergey Masserow <masserow@mail.ru>
2023-04-20 21:53:44 +0300

    first commit

---
+++ src/main/java/ru/netology/service/CashBackHacker.java
@@ -0,0 +1,8 @@
+package ru.netology.service;
+public class CashBackHacker {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+}
