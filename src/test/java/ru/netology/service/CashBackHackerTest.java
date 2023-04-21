d840dac80ed119b772087ec40ede025cc114c220
Sergey Masserow <masserow@mail.ru>
2023-04-21 19:34:36 +0300

    add junit4

---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,62 @@
+package ru.netology.service;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class CashBackHackerTest {
+
+    @Test
+    public void remain () {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remain1 () {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remain2 () {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remain3 () {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1001;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remain4 () {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+}
