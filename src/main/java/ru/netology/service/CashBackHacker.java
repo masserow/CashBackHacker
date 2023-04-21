3550801efc0c97fdedadf1d0a8efcbca11a90fa3
Sergey Masserow <masserow@mail.ru>
2023-04-21 18:05:48 +0300

    add testng

--- src/main/java/ru/netology/service/CashBackHacker.java
+++ src/main/java/ru/netology/service/CashBackHacker.java
@@ -1,6 +1,6 @@
 package ru.netology.service;
 public class CashBackHacker {
-    private final int boundary = 1000;
+    public int boundary = 1000;
 
     public int remain(int amount) {
         return boundary - amount % boundary;
